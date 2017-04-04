766463308096c2dd532d55a28e647b4ddc72ce0e
koran12@126.com <koran12@126.com>
2017-04-04 23:59:21 +0800

    init

--- judge.php
+++ judge.php
@@ -13,7 +13,7 @@ function getUser($type, $number, $prize, $name){
     }
     $total = count($result);
     if($number > $total - 5){
-        exit(json_encode(['code' => 500, 'msg' => '抽奖人数不够', 'data' => []], JSON_UNESCAPED_UNICODE));
+        exit(json_encode(['code' => 500, 'msg' => '数据不足，请稍后再抽奖！', 'data' => []], JSON_UNESCAPED_UNICODE));
     }
     $keys = getKeys($number, $total - 1);
     $list = [];
@@ -38,7 +38,7 @@ $number = isset($_POST['number']) ? $_POST['number'] : 0;
 if(!empty($prize) && !empty($type) && !empty($number) && !empty($name)) {
     $user = getUser($type, $number, $prize, $name);
     if(!empty($user)) {
-        exit(json_encode(['code' => 200, 'msg' => '获取成功', 'data' => $user], JSON_UNESCAPED_UNICODE));
+        exit(json_encode(['code' => 200, 'msg' => '抽奖成功', 'data' => $user], JSON_UNESCAPED_UNICODE));
     }
 }
-exit(json_encode(['code' => 500, 'msg' => '参数错误', 'data' => []], JSON_UNESCAPED_UNICODE));
+exit(json_encode(['code' => 500, 'msg' => '数据不足，请稍后再抽奖！', 'data' => []], JSON_UNESCAPED_UNICODE));
